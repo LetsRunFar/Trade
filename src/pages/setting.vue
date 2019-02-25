85ce01752beeafa80585e2d0dee22a9fbe4c9953
lixin <646239828@qq.com>
2019-02-25 23:21:35 +0800

    设置页面

---
+++ src/pages/setting.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="setting-wrap">
+    <x-header class="header" :left-options="{backText: ''}" fixed>用户设置</x-header>
+    <group>
+      <cell class="links" commonColor title="邮箱绑定" is-link></cell>
+    </group>
+  </div>
+</template>
+
+<script>
+  import {XHeader, Cell, Group} from 'vux'
+  export default {
+    name: "setting",
+    components: {XHeader, Cell, Group}
+  }
+</script>
+
+<style lang="less" scoped>
+  .setting-wrap{
+    padding-top: 46px;
+    min-height: 100vh;
+    background-color: #050c1e;
+    .header {
+      z-index: 10;
+
+      /deep/ .vux-header-title {
+        font-size: .427rem;
+      }
+
+      /deep/ .left-arrow:before {
+        border-width: 2px 0 0 2px;
+      }
+    }
+    .weui-cells{
+      &:after{
+        border-bottom: none;
+      }
+      &:before{
+        border-top: none;
+      }
+    }
+    .links{
+      text-align: left;
+      color: #fff;
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/login'
 import Order from '@/pages/order'
+import Setting from '@/pages/setting'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/order',
       name: 'order',
       component: Order
+    },
+    {
+      path: '/setting',
+      name: 'setting',
+      component: Setting
     }
   ]
 })
